454576ae64087311a01a1abdb2ca9ec819424385
winty <18814127231@163.com>
2019-08-10 23:48:49 +0800

    test

--- code/bundleBuild/testExport.js
+++ code/bundleBuild/testExport.js
@@ -5,5 +5,5 @@ const code = exportCode('./src/index.js')
 // const exportGraph = require('./exportGraph')
 // console.log(exportGraph('./src/index.js'))
 
-// const exportDependencies = require('./exportDependencies')
-// console.log(exportDependencies('./src/index.js'))
+const exportDependencies = require('./exportDependencies')
+console.log(exportDependencies('./src/index.js'))

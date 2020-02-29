791d69257fcb0fa92839c02f1aa0c50137410f33
winty <18814127231@163.com>
2020-02-29 15:12:46 +0800

    feat: add code

--- code/testNode.js
+++ code/testNode.js
@@ -1,12 +1,43 @@
 // setImmediate(() => console.log('timeout1'));
-setImmediate(() => {
-    console.log('timeout1')
-    Promise.resolve().then(() => console.log('promise resolve'))
-    process.nextTick(() => console.log('next tick1'))
-});
-setImmediate(() => {
-    console.log('timeout2')
-    process.nextTick(() => console.log('next tick2'))
-});
-setImmediate(() => console.log('timeout3'));
-setImmediate(() => console.log('timeout4'));
+// setImmediate(() => {
+//     console.log('timeout1')
+//     Promise.resolve().then(() => console.log('promise resolve'))
+//     process.nextTick(() => console.log('next tick1'))
+// });
+// setImmediate(() => {
+//     console.log('timeout2')
+//     process.nextTick(() => console.log('next tick2'))
+// });
+// setImmediate(() => console.log('timeout3'));
+// setImmediate(() => console.log('timeout4'));
+
+async function async1(){
+    console.log('async1 start')
+    await async2()
+    console.log('async1 end')
+}
+async function async2(){
+    console.log('async2')
+}
+console.log('script start')
+setTimeout(function(){
+    console.log('setTimeout0')
+},0)
+setTimeout(function(){
+    console.log('setTimeout3')
+},3)
+setImmediate(() => console.log('setImmediate'));
+process.nextTick(() => console.log('nextTick'));
+async1();
+new Promise(function(resolve){
+    console.log('promise1')
+    resolve();
+    console.log('promise2')
+}).then(function(){
+    console.log('promise3')
+})
+console.log('script end')
+
+ //script start=>async1 start=>async2=>promise1=>promise2
+//  =>script end=>nextTick=>async1 end=>promise3=>setTimeout0
+// =>setImmediate=>setTimeout3

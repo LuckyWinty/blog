b4c83d46ee79007354a2ce3e4e15f41fcbea91db
winty <18814127231@163.com>
2020-03-02 15:58:09 +0800

    feat: add node detail

--- code/testNode.js
+++ code/testNode.js
@@ -50,8 +50,33 @@
 //     process.nextTick(tick)
 //   })
 
-const promise = Promise.resolve()
-  .then(() => {
-    return promise
+// const promise = Promise.resolve()
+//   .then(() => {
+//     return promise
+//   })
+// promise.catch(console.error)
+setImmediate(() => {
+  console.log(1)
+  setTimeout(() => {
+    console.log(2)
+  }, 100)
+  setImmediate(() => {
+    console.log(3)
   })
-promise.catch(console.error)
+  process.nextTick(() => {
+    console.log(4)
+  })
+})
+process.nextTick(() => {
+  console.log(5)
+  setTimeout(() => {
+    console.log(6)
+  }, 100)
+  setImmediate(() => {
+    console.log(7)
+  })
+  process.nextTick(() => {
+    console.log(8)
+  })
+})
+console.log(9)

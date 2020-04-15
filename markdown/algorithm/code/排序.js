b32674beb0f18a39ca51a59bc87abcdf3c2c4ed8
winty <18814127231@163.com>
2020-04-16 00:33:34 +0800

    feat: add head top k

--- markdown/algorithm/code/排序.js
+++ markdown/algorithm/code/排序.js
@@ -92,14 +92,71 @@ var findKthLargest = function(nums, k) {
     }
 };
 // 堆排序
-// 构造初始堆
-// 调整
-// 找到第一个非叶子节点，由下而上开始调整
+/** 
+ * 构造初始堆
+ * 调整
+ * 找到第一个非叶子节点，由下而上开始调整
+*/ 
 function headSort(arr){
-    function adjustHead(arr,lastIndex){
+    creatHead(arr) // 构造初始堆
 
+    for(let i = arr.length - 1 ;i > 0 ;i--){
+        [arr[0],arr[i]] = [arr[i],arr[0]] // 元素交换
+        adjustHead(arr,0,i)
+    }
+    return arr
+
+    function adjustHead(arr,index,len){
+        for(let i = 2 * index + 1;i < len;i = 2 * i + 1){//下一个要判断的节点为当前节点的左孩子
+            if(i + 1 < len && arr[i+1]>arr[i]){ //判断左右节点中最大的节点
+                i++;
+            }
+
+            if(arr[i] > arr[index]){
+                [arr[i],arr[index]]=[arr[index],arr[i]]
+                index = i; // 谁交换则谁成为下一个要调整的节点
+            }else{
+                break; //没发生交换，则调整完成
+            }
+        }
     }
     function creatHead(arr){
-        
+        let curIndex = parseInt(arr.length/2) - 1
+        for(let i = curIndex;i >= 0;i--){
+            adjustHead(arr,i,arr.length)
+        }
     }
 }
+// 堆排序 top k
+var findKthLargest = function(arr, k) {
+    creatHead(arr)
+    for(let i = arr.length - 1;i >= 0;i--){
+        [arr[0],arr[i]] = [arr[i],arr[0]]
+        if(i <= arr.length - k){ // top 即 第 length - k 个
+            return arr[arr.length - k]
+        }else{
+            adjustHead(arr,0,i)
+        }
+    }
+
+    function adjustHead(arr,index,len){
+        for(let i = 2 * index + 1;i < len;i = 2 * index +1){
+            if(i + 1 < len && arr[i+1]>arr[i]){
+                i++
+            }
+            if(arr[i] > arr[index]){
+                [arr[i],arr[index]] = [arr[index],arr[i]]
+                index = i;                
+            }else{
+                break;
+            }
+        }
+    }
+
+    function creatHead(arr){
+        const targetIndex = parseInt(arr.length/2) - 1;
+        for(let i = targetIndex; i >= 0;i--){
+            adjustHead(arr,i,arr.length)
+        }
+    }
+};
